5be6ad50457d641b46d9e5422035571377e9cfb7
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2023-02-14 16:52:34 -0300

    adicionando a busca dos lancamentos na API

---
+++ src/app/service/lancamentoService.js
@@ -0,0 +1,59 @@
+import ApiService from "../apiService";
+
+export default class LancamentoService extends ApiService{
+    
+    constructor(){
+        super('/api/lancamentos')
+    }
+
+    obterListaMeses()  {
+        return [
+
+            {    label: 'Selecione...' , value: '' },
+            {    label: 'Janeiro' ,      value: 1  },
+            {    label: 'Fevereiro' ,    value: 2  },
+            {    label: 'Março' ,        value: 3  },
+            {    label: 'Abril' ,        value: 4  },
+            {    label: 'Maio' ,         value: 5  },
+            {    label: 'Junho' ,        value: 6  },
+            {    label: 'Julho' ,        value: 7  },
+            {    label: 'Agosto' ,       value: 8  },
+            {    label: 'Setembro' ,     value: 9  },
+            {    label: 'Outubro' ,      value: 10 },
+            {    label: 'Novembro' ,     value: 11 },
+            {    label: 'Dezembro' ,     value: 12 },
+            
+        ]
+    }
+    obterTipos() {
+        return [
+
+            {    label: 'Selecione...' , value: '' },
+            {    label: 'Despesa' ,      value: 'DESPESA'  },
+            {    label: 'Receita' ,   value: 'RECEITA' },           
+        ]
+    } 
+
+    consultar(LancamentoFiltro){
+
+        //api/lancamentos?ano=2019&mes=1&usuario=4
+        let params = `?ano=${LancamentoFiltro.ano}`
+
+        if(LancamentoFiltro.mes){
+            params = `${params}&mes=${LancamentoFiltro.mes}`
+        }
+        if(LancamentoFiltro.tipo){
+            params = `${params}&tipo=${LancamentoFiltro.tipo}`
+        }
+        if(LancamentoFiltro.status){
+            params = `${params}&status=${LancamentoFiltro.status}`
+        }
+        if(LancamentoFiltro.usuario){
+            params = `${params}&usuario=${LancamentoFiltro.usuario}`
+        }
+        if(LancamentoFiltro.descricao){
+            params = `${params}&descricao=${LancamentoFiltro.descricao}`
+        }
+        return this.get(params)
+    }
+}
--- src/views/lancamentos/consultaLancamento.js
+++ src/views/lancamentos/consultaLancamento.js
@@ -5,40 +5,60 @@ import Card from "../../components/card";
 import SelectMenu from "../../components/selectMenu";
 import LancamentoTable from "./lancamentoTable";
 
+import LancamentoService from "../../app/service/lancamentoService";
+import LocalHistorageService from "../../app/service/localstorageService";
+
+import * as messages from '../../components/toastr'
+
 class ConsultaLancamentos extends React.Component{
+
+    constructor(){
+
+        super();
+        this.lancamentoService = new LancamentoService();
+
+    }
+
+    state = {
+        ano: '',
+        mes: '',
+        tipo: '',
+        descricao:'',
+        lancamentos: []
+    }
+
+    buscar = () =>{
+        if(!this.state.ano){
+            messages.mensagemErro('O campo Ano é obrigatorio.')
+            return false
+        }
+        const usuarioLogado = LocalHistorageService.obterItem('_usuario_logado')
+
+        const lancamentoFiltro = {
+            ano: this.state.ano,
+            mes: this.state.mes,
+            tipo: this.state.tipo,
+            descricao: this.state.descricao,
+            usuario: usuarioLogado.id
+        }
+
+        this.lancamentoService
+            .consultar(lancamentoFiltro)
+            .then( response =>{
+                
+                this.setState({ lancamentos : response.data})
+              
+            }).catch( error =>{
+                console.log(error)
+            })
+
+    }
 
     render(){
 
-        const meses = [
-            
-            {    label: 'Selecione...' , value: '' },
-            {    label: 'Janeiro' ,      value: 1  },
-            {    label: 'Fevereiro' ,    value: 2  },
-            {    label: 'Março' ,        value: 3  },
-            {    label: 'Abril' ,        value: 4  },
-            {    label: 'Maio' ,         value: 5  },
-            {    label: 'Junho' ,        value: 6  },
-            {    label: 'Julho' ,        value: 7  },
-            {    label: 'Agosto' ,       value: 8  },
-            {    label: 'Setembro' ,     value: 9  },
-            {    label: 'Outubro' ,      value: 10 },
-            {    label: 'Novembro' ,     value: 11 },
-            {    label: 'Dezembro' ,     value: 12 },
-                
-        ]
+        const meses =this.lancamentoService.obterListaMeses();
 
-        const tipos = [
-            {    label: 'Selecione...' , value: '' },
-            {    label: 'Despesa' ,      value: 'DESPESA'  },
-            {    label: 'Receita' ,   value: 'RECEITA' },
-            
-        ]
-        
-        const lancamentos = [
-            { id: 1, descricao: 'Salario' , valor: 5000, mes: 1, tipo: 'Receita' , status: 'Efetivado' }
-        ]
-
-
+        const tipos = this.lancamentoService.obterTipos();
 
         return(
             <Card title="Consulta Lancamentos">
@@ -47,35 +67,55 @@ class ConsultaLancamentos extends React.Component{
                     <div className="col-md-5">
                         <div className="bs-component">
 
-                            <FormGroup htmlFor="inputAno" label="Ano: *">
+                            <FormGroup htmlFor="inputAno" label="Ano: ">
                             <input type="text" 
                                 className="form-control" 
-                                id="inputAno" 
-                                aria-describedby="emailHelp" 
+                                id="inputAno"
+                                value={this.state.ano}
+                                onChange={ e => this.setState({ ano: e.target.value})}                               
                                 placeholder="Digite o Ano" />
                             </FormGroup>
 
-                            <FormGroup htmlFor="inputMes" label="Mes: *">
-                                <SelectMenu input="inputMes" className="form-control" lista={meses}/>
+                            <FormGroup htmlFor="inputMes" label="Mes: ">
+                                <SelectMenu input="inputMes" 
+                                        className="form-control" 
+                                        value={this.state.mes}
+                                        onChange={e => this.setState({ mes: e.target.value})}
+                                        lista={meses}/>
                             </FormGroup>
 
-                            <FormGroup htmlFor="inputMes" label="Mes: *">
-                                <SelectMenu input="inputTipo" className="form-control" lista={tipos}/>
+                            <FormGroup htmlFor="inputDescricao" label="Descricao: ">
+                                <input type="text" 
+                                    className="form-control" 
+                                    id="inputDescricao"
+                                    value={this.state.descricao}
+                                    onChange={ e => this.setState({ descricao: e.target.value})}                               
+                                    placeholder="Digite o Ano" />
                             </FormGroup>
+
+                            <FormGroup htmlFor="inputTipo" label="Tipo: *">
+                                <SelectMenu input="inputTipo" 
+                                        className="form-control" 
+                                        value={this.state.tipo}
+                                        onChange={e => this.setState({ tipo: e.target.value})}
+                                        lista={tipos}/>
+                            </FormGroup>
+
                             <div className="card-body d-flex justify-content-center">
-                                <button type="button" className="btn btn-primary btn-lg me-2">Editar</button>
-                                <button type="button" className="btn btn-danger btn-lg me-2">Deletar</button>
+                                <button type="button" onClick={this.buscar} className="btn btn-primary btn-lg me-2">Buscar</button>
+                                <button type="button" className="btn btn-danger btn-lg me-2">Excluir</button>
                             </div>
                         
                         </div>
-
+                    </div>
+                    <div className="col-md-12">
                         
 
                         <div className="row">
                             <div className="col-md-12">
                                 <div className="bs-component">
 
-                                    <LancamentoTable lancamentos={lancamentos}></LancamentoTable>
+                                    <LancamentoTable lancamentos={this.state.lancamentos}></LancamentoTable>
 
                                 </div>
                             </div>
